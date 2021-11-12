b59bfbe24c4f393ff434d50ed2702d7c344395f3
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-12 18:55:53 -0500

    ➕ADD: The preloader was developed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 declare var particlesJS: any;
 @Component({
@@ -7,9 +7,21 @@ declare var particlesJS: any;
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  constructor(private _elementDom: ElementRef) {}
+
   ngOnInit(): void {
     particlesJS?.load('particles-js', '../assets/particles.json', () => {
       console.log('callback - particles.js config loaded');
     });
+
+    this.preloader();
+  }
+
+  public preloader() {
+    setTimeout(() => {
+      this._elementDom.nativeElement
+        .querySelector('#loader')
+        .classList.add('loader-loaded');
+    }, 3000);
   }
 }
