ae69a0f5857660cb8b2cae5d41aba4f1eea84dc3
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-12 17:39:06 -0500

    ➕ADD: The particles.js library was implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var particlesJS: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    particlesJS?.load('particles-js', '../assets/particles.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
