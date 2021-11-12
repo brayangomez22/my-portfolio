ef7daac9219b24a68ba347b437651c577f4590e5
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-12 16:17:30 -0500

    ➕ADD: the presentation was developed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, PresentationComponent],
+  exports: [HeaderComponent, PresentationComponent],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
---
+++ src/app/components/presentation/presentation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  templateUrl: './presentation.component.html',
+  styles: [
+  ]
+})
+export class PresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
