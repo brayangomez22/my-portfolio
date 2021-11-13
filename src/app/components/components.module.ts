4faf25ecd4f0330ff318ac2a242d19ded70319bf
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-13 15:09:37 -0500

    ➕ADD: The about section was developed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PresentationComponent],
-  exports: [HeaderComponent, PresentationComponent],
+  declarations: [HeaderComponent, PresentationComponent, AboutComponent],
+  exports: [HeaderComponent, PresentationComponent, AboutComponent],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
