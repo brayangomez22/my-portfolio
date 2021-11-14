a6cf14b946d3be0cbf4d0b361a749624406e6270
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-13 21:12:38 -0500

    ➕ADD: The portfolio section was developed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,7 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { TradesComponent } from './trades/trades.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { TradesComponent } from './trades/trades.component';
     AboutComponent,
     SkillsComponent,
     TradesComponent,
+    PortfolioComponent,
   ],
   exports: [
     HeaderComponent,
@@ -22,6 +24,7 @@ import { TradesComponent } from './trades/trades.component';
     AboutComponent,
     SkillsComponent,
     TradesComponent,
+    PortfolioComponent,
   ],
   imports: [CommonModule, SwiperModule],
 })
