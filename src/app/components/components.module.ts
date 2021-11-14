a1d19173cacc3519e43ff88a988fe8db182c5339
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-14 17:28:59 -0500

    ➕ADD: The banner section was developed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -9,6 +9,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { TradesComponent } from './trades/trades.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { FooterComponent } from './footer/footer.component';
     TradesComponent,
     PortfolioComponent,
     FooterComponent,
+    BannerComponent,
   ],
   exports: [
     HeaderComponent,
@@ -28,6 +30,7 @@ import { FooterComponent } from './footer/footer.component';
     TradesComponent,
     PortfolioComponent,
     FooterComponent,
+    BannerComponent,
   ],
   imports: [CommonModule, SwiperModule],
 })
