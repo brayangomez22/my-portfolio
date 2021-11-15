77671353fde04aa9d9178d51e3b9498491d222e0
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-14 20:14:33 -0500

    ➕ADD: The functionality to change the active section in the header began to be developed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HomeComponent implements OnInit {
+  public sections!: ElementRef[];
 
-  constructor() { }
+  constructor(private _elementDom: ElementRef) {}
 
   ngOnInit(): void {
+    this.sections =
+      this._elementDom.nativeElement.querySelectorAll('section[id]');
+
+    window.addEventListener('scroll', this.scrollActive);
   }
 
+  public scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    this.sections?.forEach((current: any) => {
+      const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id');
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        this._elementDom.nativeElement
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.add('nav__link-active');
+      } else {
+        this._elementDom.nativeElement
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.remove('nav__link-active');
+      }
+    });
+  }
 }
